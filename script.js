fc232892b1904bac8abe037a2b6159f98d312e7d
albright.human <sungutialbright@gmail.com>
2022-03-13 20:24:08 +0300

    add function to toggle design info

---
+++ script.js
@@ -0,0 +1,16 @@
+// user interface logic
+$(document).ready(function(){
+ $('#design').on('click', function(){
+
+     $('#design_info').toggle();
+
+//  });
+//  $('#development').on('click', function(){
+//     $('#dev_info').toggle();
+
+//  });
+//  $('#product').on('click' , function(){
+//      $('#product_info').toggle();
+//  })
+
+});
