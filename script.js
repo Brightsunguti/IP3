94305d540472bf63941723333501ed43c1fa8b50
albright.human <sungutialbright@gmail.com>
2022-03-13 22:44:05 +0300

    made changes to form layout

--- script.js
+++ script.js
@@ -5,12 +5,26 @@ $(document).ready(function(){
      $('#design_info').toggle();
 
  });
+
+
  $('#development').on('click', function(){
     $('#dev_info').toggle();
 
  });
+
+
  $('#product').on('click' , function(){
      $('#product_info').toggle();
- })
+ });
+
+
+ $('#btn').on('click' , function(){
+     $('#feedback').toggle()
+ });
 
 });
+$(document).ready(function(){
+    $('#project').hover(function(){
+        $('this').css("background-color" , "#c4c4c4")
+    });
+})
